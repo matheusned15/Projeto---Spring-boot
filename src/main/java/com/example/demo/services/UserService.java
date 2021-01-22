266e4cddd739933d755196d2374954bb1847af0c
matheusned15 <matheusned15@gmail.com>
2021-01-22 08:02:03 -0300

    User uptade

--- src/main/java/com/example/demo/services/UserService.java
+++ src/main/java/com/example/demo/services/UserService.java
@@ -31,4 +31,17 @@ public class UserService {
 	public void delete(Long id) {
 		userRepository.deleteById(id);
 	}
+
+	public User uptade(Long id, User obj) {
+		User entity = userRepository.getOne(id);
+		uptadeData(entity, obj);
+		return userRepository.save(entity);
+	}
+
+	private void uptadeData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+
+	}
 }
